cacc3472296b4ac3d7009674a3747e691b49bfc5
beeeeeeenny <598327628@qq.com>
2018-01-19 16:18:09 +0800

    修复dockerize渲染配置文件路径BUG

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-dockerize -template /code/novel-server/template/config.properties.template:/code/novel-server/src/main/resources/properties/config.properties
-dockerize -template /code/novel-server/template/novel.properties.template:/code/novel-server/src/main/resources/properties/novel.properties
+dockerize -template /code/novel-server/template/config.properties.template:${TOMCAT_HOME}/webapps/ROOT/WEB-INF/classes/properties/config.properties
+dockerize -template /code/novel-server/template/novel.properties.template:${TOMCAT_HOME}/webapps/ROOT/WEB-INF/classes/properties/novel.properties
 
 catalina.sh run
